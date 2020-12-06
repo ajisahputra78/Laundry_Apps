1b24b21660cc3e5ddfbadd6382659ce17a22c05c
aji saputro <aji.saputro@students.amikom.ac.id>
2020-12-06 16:17:34 +0700

    Initial Commit

---
+++ LaundryApps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LaundryApps
+//
+//  Created by Aji_sahputra on 06/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
